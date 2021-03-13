19c95a6e27d48049970ce53cf327cb61896b5eff
MisterKeyboard <antoine.clavier@hotmail.fr>
2021-03-13 19:17:00 +0100

    media queries 770 /1024

---
+++ page2formulaire.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alivio</title>
+    <link rel="icon" href="./img/Logo.svg">
+    <link rel="stylesheet" href="./css/stylepage2.css">
+</head>
+
+<body>
+    <!-- *********       HEADER             ********** -->
+    <header>
+        <div class="containerheader">
+            <div>
+                <img src="./img/Logo.svg" alt="Be aware">
+            </div>
+
+            <nav class="navheader">
+                <a href="#section1">Why Alivio</a>
+                <a href="#section3">Solutions</a>
+                <a href="#section4">Community</a>
+                <a href="https://business.google.com/v/sokha-royal-spa-miromesnil/015596358167222917005/fe93/_?caid=10765711192&agid=105526760385&gclid=EAIaIQobChMI96_Svr2l7wIVxIjVCh0KGwqwEBAYASAAEgJBwfD_BwE&gclid=EAIaIQobChMI96_Svr2l7wIVxIjVCh0KGwqwEBAYASAAEgJBwfD_BwE"
+                    target="_blank">Pricing</a>
+            </nav>
+
+            <div class="headerright">
+                <a href="#section8">Sign in </a>
+                <button class="start" onclick="window.location.href ='https://www.youtube.com/watch?v=k11Fy54HBoI';">
+                    Start Trial</button>
+            </div>
+        </div>
+    </header>
+
+    <section>       
+    </section>
+
+
+
+
+
+    <!-- *********       FOOTER            ********** -->
+    <footer>
+        <div class="navfooter">
+            <h5>Alivio</h5>
+            <nav>
+
+                <a href="#section1">Why Alivio</a>
+                <a href="#section3">Solutions</a>
+                <a href="#section4">Community</a>
+                <a href="https://business.google.com/v/sokha-royal-spa-miromesnil/015596358167222917005/fe93/_?caid=10765711192&agid=105526760385&gclid=EAIaIQobChMI96_Svr2l7wIVxIjVCh0KGwqwEBAYASAAEgJBwfD_BwE&gclid=EAIaIQobChMI96_Svr2l7wIVxIjVCh0KGwqwEBAYASAAEgJBwfD_BwE"
+                    target="_blank">Pricing</a>
+            </nav>
+        </div>
+    </footer>
+
+</body>
